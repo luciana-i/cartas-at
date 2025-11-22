1b4fca3ba8a01b84c6a40820db5ffad3f3a8900b
Administrator <Administrator@WIN-O7CAF1FNCVK>
2025-11-22 08:06:49 -0300

    seteo ambiente

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+   <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <div class="p-10 bg-yellow-200 text-black text-3xl font-bold">
+    Si ves esto en amarillo, Tailwind funciona 🎉
+  </div>
+</template>
